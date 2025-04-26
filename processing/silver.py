aaded3b1af69f8ff3b82c124841540ee77ff57ec
Leandro Jacques <lsrzj@yahoo.com>
2025-04-26 17:56:10 -0300

    Optmizing data cleaning and parquet file size

--- processing/silver.py
+++ processing/silver.py
@@ -1,7 +1,8 @@
+import math
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import sys
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
 
 if __name__ == "__main__":
     schema = StructType([
@@ -25,32 +26,54 @@ if __name__ == "__main__":
     spark = SparkSession.builder.appName("SilverProcessing").getOrCreate()
 
     try:
-        df = spark.read.schema(schema).json(sys.argv[1])
-        df.where(col("state_province") != col("state") & col("state_province").isNotNull()).select(col("state"), col("state_province")).show(df.count(), truncate=False)
-        exit(1)
+        df = spark.read.schema(schema).json(sys.argv[1]).cache()
         # Clean data
         df = (df
               # standardize column case
               .withColumn("brewery_type", lower(col("brewery_type")))
+              # Country and state will be used for partitioning, so we need to lowercase them to avoid creating multiple partitions for the same country/state
+              .withColumn("country", lower(col("country")))
+              .withColumn("state", lower(col("state")))
               # remove any non-numeric characters from phone
               .withColumn("phone", regexp_replace(col("phone"), r"[^\p{N}]", ""))
-              # 
+              # removing duplicate meaning columns and unifying into one as state and state_province are the same when state_province is not null
               .withColumn("state", coalesce(col("state"), col("state_province")))
-              .withColumn("street", coalesce(col("street"), col("address_1")))
+              # removing duplicate meaning columns and unifying into one as street and address_1 are the same when state_province is not null
+              .withColumn("address_1", coalesce(col("street"), col("address_1")))
+              # removing any non letter and non space characters from country
               .withColumn("country", regexp_replace(col("country"), r"[^\p{L}\p{Z}]", ""))
+              # removing any non letter, non space, non dash and non initial punctuation characters from state
               .withColumn("state", regexp_replace(col("state"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}]", ""))
-              .withColumn("street", regexp_replace(col("street"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}\p{N}&']", ""))
-              .withColumn("street", regexp_replace(col("street"), r"\p{Z}+", " "))
-              .withColumn("city", trim(col("state")))
-              .withColumn("street", trim(col("street")))
+              # removing any non letter, non space, non dash and non initial punctuation characters from city
+              .withColumn("city", regexp_replace(col("city"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}]", ""))
+              # removing any non letter, non space, non dash, non punctuation characters, characters different from [&'-,] from address_1, address_2 and address_3
+              .withColumn("address_1", regexp_replace(col("address_1"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}\p{N}&-',]", ""))
+              .withColumn("address_2", regexp_replace(col("address_2"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}\p{N}&-',]", ""))
+              .withColumn("address_3", regexp_replace(col("address_3"), r"[^\p{L}\p{Z}\p{Pd}\p{Pi}\p{N}&-',]", ""))
+              # transforming all two or more blanks into one space for address_1, address_2, address_3
+              .withColumn("address_1", regexp_replace(col("address_1"), r"\p{Z}+", " "))
+              .withColumn("address_2", regexp_replace(col("address_2"), r"\p{Z}+", " "))
+              .withColumn("address_3", regexp_replace(col("address_3"), r"\p{Z}+", " "))
+              # trimming city, address_1, address_2 and address_3 to remove any leading or trailing spaces
+              .withColumn("city", trim(col("city")))
+              .withColumn("address_1", trim(col("address_1")))
+              .withColumn("address_2", trim(col("address_2")))
+              .withColumn("address_3", trim(col("address_3")))
+              # trimming city, street, country and state to remove any leading or trailing spaces to avoid creating multiple partitions for the same country/state
               .withColumn("country", trim(col("country")))
               .withColumn("state", trim(col("state")))
-              .withcolumn("address", concat_ws(", ", col("address_1"), col("address_2"), col("address_3")))
-              .drop("address_1", "address_2", "address_3", "state_province")
+              # dropping columns that are not needed, they were unified into one
+              .drop("street", "state_province")
               )
+        # Calculating the number of output partitions to try to achieve the target output partition size to avoid small files
+        # it was generating 3.5Kb files, so we need to calculate the number of partitions to achieve 128MB files
+        targetOutputPartitionSizeMB = 128
+        parquetCompressionRatio = 0.1
+        numOutputPartitions = 0.0035 * parquetCompressionRatio / targetOutputPartitionSizeMB
 
         # Write partitioned
-        df.write.mode("overwrite") \
+        df.coalesce(math.ceil(numOutputPartitions)) \
+            .write.mode("overwrite") \
           .partitionBy("country", "state") \
           .parquet(sys.argv[2])
 
